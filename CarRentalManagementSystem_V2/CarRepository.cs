ede9ed3903bfee81487cc6b36c3a7648a69f9e59
ut01426 <ut01426@UNICOMTIC24>
2024-10-11 16:55:41 +0530

    q 13 (idk anymore)

---
+++ CarRentalManagementSystem_V2/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem_V2
+{
+    public class CarRepository
+    {
+         //connection = new SqlServerConnection("Server=(localdb)\\MSSQLLocalDB; Database = CarRentalManagement ; Trusted_Connection = True; TrustServerCertificate = True ");
+         public void AddCars(Car car)
+        {
+            using (SqlServerConnection connection =new SqlServerConnection("Server=(localdb)\\MSSQLLocalDB; Database = CarRentalManagement ; Trusted_Connection = True; TrustServerCertificate = True "))
+            {
+
+            }
+        }
+    }
+}
